32c7718cf828c4ad54eea951bf505a87c1b431c6
wzzlYwzzl <zjw123bit@126.com>
2016-05-20 17:38:35 +0800

    new file:   LICENCE 	new file:   bower.json 	new file:   build/postinstall.sh 	new file:   package.json 	new file:   src/app/assets/images/kubernetes-logo.png 	new file:   src/app/frontend/index.html

---
+++ build/postinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script will run after npm install
+
+../node_module/.bin/bower install --allow-root
+
+# The pwd cmd will return a path where the program which calls the script. 
+# Not the path of the postinstall.sh.
+GOPATH=`pwd`/.tools/go 
+go get github.com/tools/godep
